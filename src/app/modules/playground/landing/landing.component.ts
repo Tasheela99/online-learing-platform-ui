ef95ebfb3b08198a78f9677d028d67de4a22653f
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2024-06-19 13:53:54 +0530

    completed landing page

--- src/app/modules/playground/landing/landing.component.ts
+++ src/app/modules/playground/landing/landing.component.ts
@@ -20,6 +20,14 @@ export interface Slide {
   src: string;
   alt: string;
   title: string;
+  text:string;
+}
+
+export interface Student {
+  id: string;
+  name: string;
+  description: string;
+  star: number;
 }
 
 @Component({
@@ -41,20 +49,59 @@ export interface Slide {
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent implements OnInit{
+export class LandingComponent implements OnInit {
 
-  courseList:Array<any>=[];
+  courseList: Array<any> = [];
 
-  constructor(private playgroundService:PlaygroundService) {
+  constructor(private playgroundService: PlaygroundService) {
   }
 
   ngOnInit() {
     this.loadAllCourses()
 
     this.slidesStore = [
-      { id: '1', src: 'https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', alt: 'Image 1', title: 'Slide 1' },
-      { id: '2', src: 'https://images.unsplash.com/photo-1585314294023-215a865ddee1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', alt: 'Image 2', title: 'Slide 2' },
-      { id: '3', src: 'https://images.unsplash.com/photo-1585314540237-13cb52fe9998?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', alt: 'Image 3', title: 'Slide 3' },
+      {
+        id: '1',
+        src: 'https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        alt: 'Image 1',
+        title: 'Slide 1',
+        text: "Online Learning Platform"
+      },
+      {
+        id: '2',
+        src: 'https://images.unsplash.com/photo-1585314294023-215a865ddee1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        alt: 'Image 2',
+        title: 'Slide 2',
+        text: "Online Learning Platform"
+      },
+      {
+        id: '3',
+        src: 'https://images.unsplash.com/photo-1585314540237-13cb52fe9998?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        alt: 'Image 3',
+        title: 'Slide 3',
+        text: "Online Learning Platform"
+      },
+    ];
+
+    this.students = [
+      {
+        id: '1',
+        name: 'John Smith',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus dolore eaque excepturi facere impedit incidunt nobis quos sint totam? Accusantium architecto at eligendi nihil. Ex harum non numquam unde.\n',
+        star: 5
+      },
+      {
+        id: '2',
+        name: 'Chris Evens',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus dolore eaque excepturi facere impedit incidunt nobis quos sint totam? Accusantium architecto at eligendi nihil. Ex harum non numquam unde.\n',
+        star: 4
+      },
+      {
+        id: '3',
+        name: 'Tony Stark',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus dolore eaque excepturi facere impedit incidunt nobis quos sint totam? Accusantium architecto at eligendi nihil. Ex harum non numquam unde.\n',
+        star: 5
+      },
     ];
   }
 
@@ -64,24 +111,18 @@ export class LandingComponent implements OnInit{
     });
   }
 
-  enrollForCourse(_id: any) {
-
-  }
-
   slidesStore: Slide[] = [];
+  students: Student[] = [];
 
-
-
-
-  customOptions: OwlOptions = {
-    autoplay:true,
+  studentOptions: OwlOptions = {
+    autoplay: true,
     loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
     navSpeed: 700,
-    navText: ['', ''],
+    margin:5,
     responsive: {
       0: {
         items: 1
@@ -93,9 +134,35 @@ export class LandingComponent implements OnInit{
         items: 3
       },
       940: {
+        items: 3
+      }
+    },
+  }
+
+
+  customOptions: OwlOptions = {
+    animateIn: 'animate__animated animate__fadeIn',
+    animateOut: 'slider-image animate__animated animate__fadeOut',
+    autoplay: true,
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
         items: 1
       }
     },
-    nav: true
   }
 }
