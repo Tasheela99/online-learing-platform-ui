02a59ed2a4c73f984758b01d7e82cf2868f9cc43
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2024-06-19 23:26:05 +0530

    finalize

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
@@ -7,7 +7,13 @@ import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {httpManagerInterceptor} from "./interceptor/http-manager.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(
-    withInterceptors([httpManagerInterceptor]),
-  ),]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes, withHashLocation()),
+    provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([
+        httpManagerInterceptor]
+      ),
+    ),]
 };
